195cee63678c3383833a1fd63d4fbecd709dd1b1
David McNally <david.mcnally@dsl.pipex.com>
2009-02-12 19:54:44 +0000

    reorganisation

--- generators/designer_friendly/designer_friendly_generator.rb
+++ generators/designer_friendly/designer_friendly_generator.rb
@@ -1,8 +1,13 @@
 class DesignerFriendlyGenerator < Rails::Generator::NamedBase
   def manifest
+
+    destination_path = "/app/views/forms/#{file_path}"
+
     record do |m|
-      # m.directory "lib"
-      # m.template 'README', "README"
+      m.directory destination_path
+      m.template 'forms/general.html.erb', "#{destination_path}/general.html.erb"
+      m.template 'forms/general_with_errors.html.erb', "#{destination_path}/general_with_errors.html.erb"
+
     end
   end
 end
