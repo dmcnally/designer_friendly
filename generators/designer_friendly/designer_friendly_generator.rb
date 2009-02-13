9164dbfe50357350f34d70a176810f96e580301a
David McNally <david.mcnally@dsl.pipex.com>
2009-02-13 21:52:46 +0000

    first rough cut

--- generators/designer_friendly/designer_friendly_generator.rb
+++ generators/designer_friendly/designer_friendly_generator.rb
@@ -7,8 +7,8 @@ class DesignerFriendlyGenerator < Rails::Generator::NamedBase
 
     record do |m|
       m.directory destination_path
-      m.template 'views/forms/general.html.erb', "#{destination_path}/general.html.erb"
-      m.template 'views/forms/general_with_errors.html.erb', "#{destination_path}/general_with_errors.html.erb"
+      m.template 'views/forms/general.html.erb', "#{destination_path}/_general.html.erb"
+      m.template 'views/forms/general_with_errors.html.erb', "#{destination_path}/_general_with_errors.html.erb"
       m.template 'models/form_builder.rb', "/app/models/#{file_path}_form_builder.rb", :assigns => {:class_name => class_name}
     end
   end
