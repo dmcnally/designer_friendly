f41c2dce4df9086b00eaf3081bc5f63d6e76a76d
David McNally <david.mcnally@dsl.pipex.com>
2009-02-16 19:57:33 +0000

    Version 0.01 with ass covering README

--- generators/designer_friendly/designer_friendly_generator.rb
+++ generators/designer_friendly/designer_friendly_generator.rb
@@ -14,8 +14,6 @@ class DesignerFriendlyGenerator < Rails::Generator::NamedBase
       m.template 'views/forms/_general.html.erb', "#{destination_path}/_text_area.html.erb"
       m.template 'views/forms/_general_with_errors.html.erb', "#{destination_path}/_test_area_with_errors.html.erb"
 
-
-
       m.template 'models/form_builder.rb', "/app/models/#{file_path}_form_builder.rb", :assigns => {:class_name => class_name}
     end
   end
